eb963715ff7f5b3a717cecc8d04e9517833f565f
Saran S <sarancad01@brightskills.com>
2024-02-19 16:39:03 +0530

    add some csharp files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+/*
+class saran{
+    static void Main(string[]args){
+        int s1=10;
+        double s2=2.0;
+        bool s3=true;
+        Console,WriteLine(Convert.ToString(s1));
+    }
+}
+*/
+
+
+
+ class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter username:");
+      string userName = Console.ReadLine();
+      Console.WriteLine("Username is: " + userName);
+    }
+  }
+
