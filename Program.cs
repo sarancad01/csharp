631a9365efb3ed247233b53874cabc0a64721bc9
Saran S <sarancad01@brightskills.com>
2024-02-21 18:04:14 +0530

    add some files

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ class saran{
 */
 
 
-
+/*
  class Program
   {
     static void Main(string[] args)
@@ -21,4 +21,85 @@ class saran{
       Console.WriteLine("Username is: " + userName);
     }
   }
+*/
 
+/*
+   
+class Program 
+    {
+        static void showmsg3(){
+            Console.WriteLine("something about saran");
+            showmsg1();
+        }
+
+
+        static void showmsg(){
+            Console.WriteLine("saran");
+            showmsg3();
+        }
+
+
+        static void showmsg2(int x){
+            Console.WriteLine("and also saran"+ x);
+        }
+
+
+
+        static void showmsg1(){
+            Console.WriteLine("If any doubt in saran");
+            showmsg2(10);
+        }
+
+
+
+
+        static string returnmethod(){
+            string name = "saran";
+            return name;
+        }
+
+
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("hi da, how are you");
+            Console.WriteLine("hope u doing well");
+            showmsg();
+            returnmethod();
+        }
+    }
+
+*/
+/*
+ class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Enter username:");
+      string userName = Console.ReadLine();
+      Console.WriteLine("Username is: " + userName);
+    }
+  }
+*/
+
+/*
+class Program{
+  static void Main(string[]args){
+     Console.WriteLine("added value of a and b is " + add(4,5));
+     Console.WriteLine("add value of a n"+sub(5,6));
+  }
+  static int add(int a, int b){
+    return a+b;
+  }
+}
+*/
+
+class Program{
+  static void Main(strings()args)
+  {
+    var sira=Console.ReadLine();
+    var l="l1";
+
+    if sira=="li";
+    
+  }
+}
---
+++ inherit/Program.cs
@@ -0,0 +1,85 @@
+﻿
+/*
+  // base class
+  class Animal { 
+
+    public string name;
+
+    public void display() {
+      Console.WriteLine("I am an animal");
+    }
+    
+  } 
+  
+  // derived class of Animal 
+  class Dog : Animal {
+    
+    public void getName() {
+      Console.WriteLine("My name is " + name);
+    }
+  }
+
+  class Program {
+
+    static void Main(string[] args) {
+
+      // object of derived class
+      Dog labrador = new Dog();
+
+      // access field and method of base class
+      labrador.name = "sira";
+      labrador.display();
+
+      // access method from own class
+      labrador.getName();
+
+      Console.ReadLine();
+    }
+
+  }
+*/
+
+class Kalaingar{
+
+    public string location;
+    
+    public Kalaingar()
+    {
+        Console.WriteLine("Welcome to Kalaingar family");
+    }
+    public string partyname(string ppartyname){
+        
+        return "I have Political Party " + ppartyname ;
+
+    }
+
+
+}
+
+class mkstalin : Kalaingar
+{
+     public mkstalin(string name){
+        location = "gopalapuram";
+        Console.WriteLine(partyname(name) + " " + location);
+     }
+}
+
+
+class mkazhagiri : Kalaingar
+{
+     public mkazhagiri(string name){
+         location = "Madurai";
+         Console.WriteLine(partyname(name) + " " + location);
+     }
+}
+
+class family{
+
+    static void Main(string [] args){
+       // Kalaingar kr=new Kalaingar();
+       mkstalin stalin=new mkstalin("dmk");
+       mkazhagiri azhagiri=new mkazhagiri("dmk");
+        
+
+    }
+}
